39a09acd29c1ac5f63a48b4404f84f1ee7d716ab
soasurs <soasurs@gmail.com>
2026-04-18 15:17:44 +0800

    feat: add cloudflare plugin
    Signed-off-by: soasurs <soasurs@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vitest/config";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [tailwindcss(), reactRouter(), cloudflare()],
   resolve: {
     tsconfigPaths: true,
   },
